3cee08eff4a616d5a0c9fdc0f60a3110fee768cb
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-02-09 10:44:17 +0500

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
       },
+      fontSize: {
+        '15': ['0.9375rem', '1.125rem'],
+      },
       colors: {
         dark: '#1A1A1A',
+        secondary: '#343434',
         brand: {
           'gray-100': '#E0E0E0',
+          'gray-800': '#3E3E3E',
+          'yellow-400': '#EECB6D',
+          'yellow-600': '#C59D3E',
         }
       },
       spacing: {
@@ -18,7 +25,7 @@ module.exports = {
         70: '4.375rem',
         100: '6.25rem',
         520: '32.5rem',
-        base: '75rem',
+        base: '86.5rem',
       }
     },
   },
